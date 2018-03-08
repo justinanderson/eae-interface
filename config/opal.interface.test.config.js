2b58a7115dc69f91935bd33888365b3c18bae5a5
Axel Oehmichen <axelfrancois.oehmichen11@imperial.ac.uk>
2018-03-08 16:04:25 +0000

    cleanup and missing pieces. to be continued

--- config/opal.interface.test.config.js
+++ config/opal.interface.test.config.js
@@ -3,6 +3,6 @@ module.exports = {
     mongoURL: 'mongodb://mongodb/eae',
     port: 80,
     enableCors: true,
-    algoServiceURL: 'http://algoService:3000',
+    algoServiceURL: 'http://algoservice:3000',
     algorithmsDirectory: 'algorithms'
 };
---
+++ test/opal.algoservice.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    mongoURL: 'mongodb://mongodb/opal',
+    port: 3001,
+	enableCors: true
+};
